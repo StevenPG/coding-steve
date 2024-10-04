b434fef2535748d3ab2759e8d7176df47aba083c
Steven Gantz <steven.p.gantz@gmail.com>
2024-10-04 18:13:25 -0400

    another migration option

--- astro/astro-simple-blog-theme-main/src/lib/utils/sortFunctions.ts
+++
@@ -1,9 +0,0 @@
-// sort by date
-export const sortByDate = (array: any[]) => {
-  const sortedArray = array.sort(
-    (a: any, b: any) =>
-      new Date(b.data.date && b.data.date) -
-      new Date(a.data.date && a.data.date)
-  );
-  return sortedArray;
-};
